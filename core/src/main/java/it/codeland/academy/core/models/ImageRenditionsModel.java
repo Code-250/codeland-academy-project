67526257d0dbefbb738fd1e3d162b86908b654cf
Richard Munyemana <richmunye@gmail.com>
2022-12-06 12:05:10 +0200

    atmic components and rendiitions

--- core/src/main/java/it/codeland/academy/core/models/ImageRenditionsModel.java
+++ core/src/main/java/it/codeland/academy/core/models/ImageRenditionsModel.java
@@ -1,6 +1,5 @@
 package it.codeland.academy.core.models;
 
-
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.jcr.Node;
@@ -28,33 +27,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import it.codeland.academy.core.utils.*;
 
-@Model(adaptables = {SlingHttpServletRequest.class, Resource.class},
-        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = { SlingHttpServletRequest.class,
+		Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 
 public class ImageRenditionsModel {
 
-    @SlingObject
-    private ResourceResolver resourceResolver;
+	@SlingObject
+	private ResourceResolver resourceResolver;
 
-    @ScriptVariable
-    Page currentPage;
+	@ScriptVariable
+	Page currentPage;
 
-    @SlingObject
-    private Resource currentResource;
+	@SlingObject
+	private Resource currentResource;
 
-    @Inject
-    @Source("osgi-services")
-    protected MimeTypeService mimeTypeService;
+	@Inject
+	@Source("osgi-services")
+	protected MimeTypeService mimeTypeService;
 
-    @RequestAttribute
+	@RequestAttribute
 	@Optional
 	private String fileReference;
 
-    @RequestAttribute
+	@RequestAttribute
 	@Optional
 	private String fileRefMobile;
 
-    @RequestAttribute
+	@RequestAttribute
 	@Optional
 	private String desktopRend;
 
@@ -70,84 +69,82 @@ public class ImageRenditionsModel {
 	@Optional
 	private String mobileRendx2;
 
-    String desktopRenditionPath;
-    String mobileRenditionPath;
-    String desktopRetinaRenditionPath;
-    String mobileRetinaRenditionPath;
+	String desktopRenditionPath;
+	String mobileRenditionPath;
+	String desktopRetinaRenditionPath;
+	String mobileRetinaRenditionPath;
 
-    protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
-    @PostConstruct
-    public void init() {
+	@PostConstruct
+	public void init() {
 
-    	if(StringUtils.isNotBlank(fileReference)) {
+		if (StringUtils.isNotBlank(fileReference)) {
 
-    		if(StringUtils.isBlank(fileRefMobile)) {
-    			fileRefMobile = fileReference;
+			if (StringUtils.isBlank(fileRefMobile)) {
+				fileRefMobile = fileReference;
 
-    			if(StringUtils.isNotBlank(desktopRend)){
-    				this.desktopRenditionPath = this.getPrimaryImageRenditions(fileReference, desktopRend);
+				if (StringUtils.isNotBlank(desktopRend)) {
+					this.desktopRenditionPath = this.getPrimaryImageRenditions(fileReference, desktopRend);
 				}
-    			if(StringUtils.isNotBlank(mobileRend)){
-    				this.mobileRenditionPath = this.getPrimaryImageRenditions(fileRefMobile, mobileRend);
+				if (StringUtils.isNotBlank(mobileRend)) {
+					this.mobileRenditionPath = this.getPrimaryImageRenditions(fileRefMobile, mobileRend);
 				}
-    			if(StringUtils.isNotBlank(desktopRendx2)){
-    				this.desktopRetinaRenditionPath = this.getPrimaryImageRenditions(fileReference, desktopRendx2);
+				if (StringUtils.isNotBlank(desktopRendx2)) {
+					this.desktopRetinaRenditionPath = this.getPrimaryImageRenditions(fileReference, desktopRendx2);
 				}
-    			if(StringUtils.isNotBlank(mobileRendx2)){
-    				this.mobileRetinaRenditionPath = this.getPrimaryImageRenditions(fileRefMobile, mobileRendx2);
+				if (StringUtils.isNotBlank(mobileRendx2)) {
+					this.mobileRetinaRenditionPath = this.getPrimaryImageRenditions(fileRefMobile, mobileRendx2);
 				}
-    		}
+			}
 
-    	}else {
-	        if (!ResourceUtil.isSyntheticResource(currentResource)) {
-	            String desktopRendition = StringUtils.EMPTY;
-	            String mobileRendition = StringUtils.EMPTY;
-	            String desktopRenditionR = StringUtils.EMPTY;
-	            String mobileRenditionR = StringUtils.EMPTY;
-	            ContentPolicyManager policyManager = resourceResolver.adaptTo(ContentPolicyManager.class);
-	            try {
-	                if (policyManager != null) {
-	                    ContentPolicy contentPolicy = policyManager.getPolicy(this.currentResource);
-	                    if (contentPolicy != null) {
-	                        switch (JcrUtils.getStringProperty(currentResource.adaptTo(Node.class),
-	                                "sling:resourceType", "")) {
-	                            default:
-	                                desktopRendition = contentPolicy.getProperties().get("desktop", StringUtils.EMPTY);
-	                                mobileRendition = contentPolicy.getProperties().get("mobile", StringUtils.EMPTY);
-	                                desktopRenditionR = contentPolicy.getProperties().get("desktopRetina", StringUtils.EMPTY);
-	                                mobileRenditionR = contentPolicy.getProperties().get("mobileRetina", StringUtils.EMPTY);
-	                        }
-	                    }
-	                }
-	                String assetUrl =
-	                        JcrUtils.getStringProperty(currentResource.adaptTo(Node.class), "fileReference", "");
-	                String assetUrlMob =
-	                        JcrUtils.getStringProperty(currentResource.adaptTo(Node.class), "fileRefMobile", "");
-	                this.desktopRenditionPath = this.getPrimaryImageRenditions(assetUrl, desktopRendition);
-	                this.mobileRenditionPath = this.getPrimaryImageRenditions(assetUrlMob, mobileRendition);
-	                this.desktopRetinaRenditionPath = this.getPrimaryImageRenditions(assetUrl, desktopRenditionR);
-	                this.mobileRetinaRenditionPath = this.getPrimaryImageRenditions(assetUrlMob, mobileRenditionR);
-	            } catch (RepositoryException e) {
-	                e.printStackTrace();
-	            }
-	        }
-    	}
-    }
+		} else {
+			if (!ResourceUtil.isSyntheticResource(currentResource)) {
+				String desktopRendition = StringUtils.EMPTY;
+				String mobileRendition = StringUtils.EMPTY;
+				String desktopRenditionR = StringUtils.EMPTY;
+				String mobileRenditionR = StringUtils.EMPTY;
+				ContentPolicyManager policyManager = resourceResolver.adaptTo(ContentPolicyManager.class);
+				try {
+					if (policyManager != null) {
+						ContentPolicy contentPolicy = policyManager.getPolicy(this.currentResource);
+						if (contentPolicy != null) {
+							switch (JcrUtils.getStringProperty(currentResource.adaptTo(Node.class),
+									"sling:resourceType", "")) {
+								default:
+									desktopRendition = contentPolicy.getProperties().get("desktop", StringUtils.EMPTY);
+									mobileRendition = contentPolicy.getProperties().get("mobile", StringUtils.EMPTY);
+									desktopRenditionR = contentPolicy.getProperties().get("desktopRetina", StringUtils.EMPTY);
+									mobileRenditionR = contentPolicy.getProperties().get("mobileRetina", StringUtils.EMPTY);
+							}
+						}
+					}
+					String assetUrl = JcrUtils.getStringProperty(currentResource.adaptTo(Node.class), "fileReference", "");
+					String assetUrlMob = JcrUtils.getStringProperty(currentResource.adaptTo(Node.class), "fileRefMobile", "");
+					this.desktopRenditionPath = this.getPrimaryImageRenditions(assetUrl, desktopRendition);
+					this.mobileRenditionPath = this.getPrimaryImageRenditions(assetUrlMob, mobileRendition);
+					this.desktopRetinaRenditionPath = this.getPrimaryImageRenditions(assetUrl, desktopRenditionR);
+					this.mobileRetinaRenditionPath = this.getPrimaryImageRenditions(assetUrlMob, mobileRenditionR);
+				} catch (RepositoryException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    public String getPrimaryImageRenditions(String imagePath, String rendition) {
+	public String getPrimaryImageRenditions(String imagePath, String rendition) {
 
-        Resource previewResource = resourceResolver.getResource(imagePath);
-        return Rendition.getRenditionByPolicy(previewResource, rendition, mimeTypeService);
-    }
+		Resource previewResource = resourceResolver.getResource(imagePath);
+		return Rendition.getRenditionByPolicy(previewResource, rendition, mimeTypeService);
+	}
 
-    public String getDesktopRenditionPath() {
-        return desktopRenditionPath;
-    }
+	public String getDesktopRenditionPath() {
+		return desktopRenditionPath;
+	}
 
-    public String getMobileRenditionPath() {
-        return mobileRenditionPath;
-    }
+	public String getMobileRenditionPath() {
+		return mobileRenditionPath;
+	}
 
 	public String getDesktopRetinaRenditionPath() {
 		return desktopRetinaRenditionPath;
